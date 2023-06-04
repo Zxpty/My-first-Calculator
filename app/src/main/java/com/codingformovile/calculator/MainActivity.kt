820c12e6c02ff46f20516766fc2892956f156359
Zepty <Anthony-08.2004@hotmail.com>
2023-06-04 02:46:16 -0500

    agrege los funciones para que el codigo funcione

--- app/src/main/java/com/codingformovile/calculator/MainActivity.kt
+++ app/src/main/java/com/codingformovile/calculator/MainActivity.kt
@@ -1,11 +1,115 @@
 package com.codingformovile.calculator
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.icu.text.DecimalFormat
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.codingformovile.calculator.databinding.ActivityMainBinding
+import org.mariuszgromada.math.mxparser.Expression
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonClear.setOnClickListener {
+            binding.input.text = ""
+            binding.output.text = ""
+        }
+
+        binding.buttonBracketLeft.setOnClickListener {
+            binding.input.text = addToInputText("(")
+        }
+        binding.buttonBracketRight.setOnClickListener {
+            binding.input.text = addToInputText(")")
+        }
+        binding.button0.setOnClickListener {
+            binding.input.text = addToInputText("0")
+        }
+        binding.button1.setOnClickListener {
+            binding.input.text = addToInputText("1")
+        }
+        binding.button2.setOnClickListener {
+            binding.input.text = addToInputText("2")
+        }
+        binding.button3.setOnClickListener {
+            binding.input.text = addToInputText("3")
+        }
+        binding.button4.setOnClickListener {
+            binding.input.text = addToInputText("4")
+        }
+        binding.button5.setOnClickListener {
+            binding.input.text = addToInputText("5")
+        }
+        binding.button6.setOnClickListener {
+            binding.input.text = addToInputText("6")
+        }
+        binding.button7.setOnClickListener {
+            binding.input.text = addToInputText("7")
+        }
+        binding.button8.setOnClickListener {
+            binding.input.text = addToInputText("8")
+        }
+        binding.button9.setOnClickListener {
+            binding.input.text = addToInputText("9")
+        }
+        binding.buttonDot.setOnClickListener {
+            binding.input.text = addToInputText(".")
+        }
+        binding.buttonDivision.setOnClickListener {
+            binding.input.text = addToInputText("÷") // ALT + 0247
+        }
+        binding.buttonMultiply.setOnClickListener {
+            binding.input.text = addToInputText("×") // ALT + 0215
+        }
+        binding.buttonSubtraction.setOnClickListener {
+            binding.input.text = addToInputText("-")
+        }
+        binding.buttonAddition.setOnClickListener {
+            binding.input.text = addToInputText("+")
+        }
+
+        binding.buttonEquals.setOnClickListener {
+            showResult()
+        }
     }
-}
+
+    private fun addToInputText(buttonValue: String): String {
+        return "${binding.input.text}$buttonValue"
+    }
+
+    private fun getInputExpression(): String {
+        var expression = binding.input.text.replace(Regex("÷"), "/")
+        expression = expression.replace(Regex("×"), "*")
+        return expression
+    }
+
+    @SuppressLint("SetTextI18n")
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showResult() {
+        try {
+            val expression = getInputExpression()
+            val result = Expression(expression).calculate()
+            if (result.isNaN()) {
+                //show message error
+                binding.output.text = "Error"
+                binding.output.setTextColor(ContextCompat.getColor(this, R.color.green))
+
+            } else {
+                //show result
+                binding.output.text =
+                    DecimalFormat("0.######").format(result).toString()
+                binding.output.setTextColor(ContextCompat.getColor(this, R.color.green))
+
+            }
+        } catch (e: Exception) {
+            // show Error message
+            binding.output.text = "Error"
+            binding.output.setTextColor(ContextCompat.getColor(this, R.color.red))}}}
